ecee620a10411133a08a335b977f973077e8a2b1
Hyunglok Kim <hk5kp@virginia.edu>
2020-07-08 15:05:29 -0400

    ex1 modified

--- Python/ex1.py
+++ Python/ex1.py
@@ -4,6 +4,7 @@ import scipy
 import random
 import numpy as np
 from matplotlib import pyplot as plt
+from subprocess import call
 os.chdir('/home/h/Git/bootcamp2020/Python')
 with open('csv.html',newline='') as f:
     reader = csv.reader(f)
@@ -19,3 +20,7 @@ for i in range(10):
     
 plt.hist(data, bins=20)
 plt.show()
+
+call('git add .', shell = True)
+call('git commit -a "commiting..."', shell = True)
+call('git push origin master', shell = True)
