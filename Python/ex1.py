db0260d8b5cacadaa23ceacc4795053fcb4678c4
Hyunglok Kim <hk5kp@virginia.edu>
2020-07-08 14:14:55 -0400

    python tutorial

--- Python/ex1.py
+++ Python/ex1.py
@@ -1,7 +1,21 @@
 import os
 import csv
+import scipy
+import random
+import numpy as np
+from matplotlib import pyplot as plt
 os.chdir('/home/h/Git/bootcamp2020/Python')
 with open('csv.html',newline='') as f:
     reader = csv.reader(f)
     for row in reader:
         print(row)
+        
+r = random.randint(1, 10)
+
+data = []
+for i in range(10):
+    print(i)
+    data.append(random.randint(1,10))
+    
+plt.hist(data, bins=20)
+plt.show()
