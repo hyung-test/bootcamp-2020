38cea05d80351333fdbfa90c3b587b35741d7f54
Hyunglok Kim <hk5kp@virginia.edu>
2020-07-08 00:12:21 -0400

    ex1.R modified

--- R/ex1.R
+++ R/ex1.R
@@ -3,3 +3,35 @@ print('hello world')
 myphrase <- "goodbye"
 class(myphrase)
 print(myphrase)
+
+c(2,4,6)
+2:6
+seq(2, 3, by=0.5)
+
+x=rep(c(2,4,6), each=3)
+print(x)
+x[3]
+table(x)
+unique(x)
+
+for (i in 1:4){
+  j <- i + 10
+  print(j)
+}
+
+print(i)
+while (i<5){
+  print(i)
+  i <- i + 1
+}
+
+if (i>3){
+  print('Yes')
+} else {
+  print('No')
+}
+
+square <-function(x){
+  squared <- x*x
+  return(squared)
+}
