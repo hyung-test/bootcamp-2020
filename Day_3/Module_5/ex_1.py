dad70f873e9be18731745ad619c774b7cdbef81f
Hyunglok Kim <hk5kp@virginia.edu>
2020-07-09 10:01:39 -0400

    source added

--- Day_3/Module_5/ex_1.py
+++ Day_3/Module_5/ex_1.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import time
 
+
 # with datetime module
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
@@ -10,3 +11,5 @@ print("Current Time is ", current_time)
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 print("Current Time is ", current_time)
+
+# sources: https://www.programiz.com/python-programming/datetime/current-time
