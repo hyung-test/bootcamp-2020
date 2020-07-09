91ecb8eda625ca348b685dc613e9b58ed0f5f316
Hyunglok Kim <hk5kp@virginia.edu>
2020-07-09 10:00:07 -0400

    Python time ex added

---
+++ Day_3/Module_5/ex_1.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import time
+
+# with datetime module
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time is ", current_time)
+
+# with time module
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+print("Current Time is ", current_time)
