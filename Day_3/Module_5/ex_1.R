677a27b639a3ebfc52886d55d6f7a51a7686a689
Hyunglok Kim <hk5kp@virginia.edu>
2020-07-09 10:14:56 -0400

    R code added

--- Day_3/Module_5/ex_1.R
+++ Day_3/Module_5/ex_1.R
@@ -1,4 +1,11 @@
-
+library(glue)
 current_time <- format(Sys.time(), "%X")
 
-print(current_time)
+# with glue paste0
+print(paste0("Current time is ", current_time))
+
+# with glue lib
+glue("Current time is: {current_time}")
+
+# with message
+message("Current time is ", current_time)
