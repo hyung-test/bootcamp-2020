224fcab99f2e803c975e2596ce96eb33f73e6920
Hyunglok Kim <hk5kp@virginia.edu>
2020-07-09 09:45:21 -0400

    Day 3

--- Python/ex1.py
+++ Python/ex1.py
@@ -3,24 +3,25 @@ import csv
 import scipy
 import random
 import numpy as np
-from matplotlib import pyplot as plt
-from subprocess import call
-os.chdir('/home/h/Git/bootcamp2020/Python')
-with open('csv.html',newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
+# =============================================================================
+# from matplotlib import pyplot as plt
+# from subprocess import call
+# os.chdir('/home/h/Git/bootcamp2020/Python')
+# with open('csv.html',newline='') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         #print(row)
+# =============================================================================
         
-r = random.randint(1, 10)
+#r = random.randint(1, 10)
 
-data = []
-for i in range(10):
-    print(i)
-    data.append(random.randint(1,10))
+#data = []
+#for i in range(10):
+    #print(i)
+#    data.append(random.randint(1,10))
     
-plt.hist(data, bins=20)
-plt.show()
+#plt.hist(data, bins=20)
+#plt.show()
 
-call('git add .', shell = True)
-call('git commit -a "commiting..."', shell = True)
-call('git push origin master', shell = True)
+a = np.random.randint(1,7,3)
+print(sum(a))
